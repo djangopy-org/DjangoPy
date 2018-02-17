830f4ed32b88a53167d9ebedf4e24213bab37a1b
jai-singhal <jaisinghal48@gmail.com>
2018-02-17 17:13:03 +0530

    soical links + post added

--- js/front.js
+++ js/front.js
@@ -1,14 +1,6 @@
 // google analytics
 var data = {};
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-98271390-1', 'auto');
-ga('send', 'pageview');
-
-
 function processPageView(rows){
     $.map(rows, function(val){
       data[val[0]] = val[1];
