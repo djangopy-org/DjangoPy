d43e8a4ca48ff898e171fcba7ad4b9322b220550
jai-singhal <jaisinghal48@gmail.com>
2018-02-18 15:52:36 +0530

    search functionality added

--- js/front.js
+++ js/front.js
@@ -45,6 +45,66 @@ $(document).ready(function () {
 
 
 
+
+
+    
+    var feedArray = [];
+    $("#search_button").click(function(){
+        $("#search").val("");
+        $("#search_results").html("");
+
+        if(feedArray.length) return false;
+
+        $.ajax({
+            type: "GET",
+            url: "feed.xml",
+            dataType: "xml",
+            success: function(returnedXMLResponse) {
+                $($(returnedXMLResponse).find('item')).each(function(index, content){
+                    var feed = {};
+                    feed['link'] = $(content).find("link").text();
+                    feed['title'] = $(content).find("title").text();
+                    feed['description'] = $(content).find("description").text();
+                    var tags = [];
+                    $($(content).find("category")).each(function(i, value){
+                        tags.push(value.innerHTML);
+                    });
+                    feed['tags'] = tags;
+                    feedArray.push(feed);
+                    
+                });
+            },
+            error: function() {
+                console.error("Failed to get search results")
+            }
+        });        
+    })
+
+
+    $("#search_form").submit(function(e){
+        e.preventDefault();
+        var search_query = $("#search").val().toLowerCase();
+        var search_results = "";
+        var count = 0;
+        for(var i = 0; i < feedArray.length; i++){
+            var q1 = feedArray[i].title.toLowerCase().search(search_query) != -1;
+            var q2 = feedArray[i].description.toLowerCase().search(search_query) != -1;
+            var q3 =  (feedArray[i].tags).indexOf(search_query) != -1; 
+            if(q1 || q2 || q3){
+                count++;
+                search_results += "<p><a href = '" + feedArray[i].link + "'>" + feedArray[i].title + "</a></p>"
+            }
+        }
+        if(count == 0){
+            search_results = "<p style = 'color:red'>No results found</p>"
+        }
+        setTimeout(function() {
+            $("#search_results").html("<br>" + search_results);
+        }, 200);
+    })
+
+
+
     // ------------------------------------------------------- //
     // For demo purposes only
     // ------------------------------------------------------ //
